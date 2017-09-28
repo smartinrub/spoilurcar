3c0f7caf106330fae278d9f3edcef21dffb3e90c
Sergio Martin <econsergio@gmail.com>
2017-09-28 16:43:54 +0200

    java structure

---
+++ src/main/java/com/sergiomartin/spoilurcar/SpoilurcarApplication.java
@@ -0,0 +1,12 @@
+package com.sergiomartin.spoilurcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoilurcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoilurcarApplication.class, args);
+	}
+}
